a300f3bdc15755b522331472c2a1da2b20a25c46
sina-ashtari004 <sinaashtari004@gmail.com>
2025-03-21 22:27:12 +0330

    Feat : Likes entity model added

---
+++ Clickly.Data/Models/Like.cs
@@ -0,0 +1,13 @@
+﻿namespace Clickly.Data.Models
+{
+    public class Like
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        // Navigations 
+        public Post Post { get; set; }
+        public User User { get; set; }
+
+    }
+}
