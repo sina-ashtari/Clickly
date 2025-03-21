8be38425a11020d42e6f4d22432f99775b5378e0
sina-ashtari004 <sinaashtari004@gmail.com>
2025-03-21 22:27:38 +0330

    Feat : Posts entity model added.

---
+++ Clickly.Data/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clickly.Data.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Content { get; set; }
+        public string? Image { get; set; }
+        public int NumberOfReport { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        // foregin key 
+        public int UserId { get; set; }
+        // Navigation Properties 
+        public User User { get; set; }
+        public ICollection<Like> Like { get; set; } = new List<Like>();
+    }
+}
