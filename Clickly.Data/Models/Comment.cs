f9639be3af9ee52483ebbf706b7e37a9e4d34bac
sina-ashtari <sinaa1383@gmail.com>
2025-04-06 18:40:42 +0330

    Feat : Add Favorite model class

---
+++ Clickly.Data/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clickly.Data.Migrations;
+
+namespace Clickly.Data.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        // Foregin keys 
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        // Navigations 
+        public Post Post { get; set; }
+        public User User { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+        public IEnumerable<Like> Likes { get; set; }
+
+
+    }
+}
