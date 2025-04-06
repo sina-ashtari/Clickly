e76a7e9f9b73ce31b822612b5a4affa3149d0ff2
sina-ashtari <sinaa1383@gmail.com>
2025-04-06 18:43:25 +0330

    Feat : Added User model class

--- Clickly.Data/Models/User.cs
+++ Clickly.Data/Models/User.cs
@@ -5,9 +5,16 @@
         public int Id { get; set; }
         public required string FullName { get; set; }
         public string? ProfilePictureUrl { get; set; }
+        public bool IsDeleted { get; set; }
+
 
         // Navigation Properties for one-to-many relationships
         public ICollection<Post> Posts { get; set; } = new List<Post>();
         public ICollection<Like> Likes { get; set; } = new List<Like>();
+        public ICollection<Story> Stories { get; set; } = new List<Story>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
+        public ICollection<Report> Reports { get; set; } = new List<Report>();
+
     }
 }
