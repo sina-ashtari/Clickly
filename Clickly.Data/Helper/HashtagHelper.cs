5132f3abf31dc18b4399338da40332858f44880e
sina-ashtari <sinaa1383@gmail.com>
2025-04-06 18:40:05 +0330

    Feat : Added Hashtag Helper

---
+++ Clickly.Data/Helper/HashtagHelper.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Clickly.Data.Helper
+{
+    public static class HashtagHelper
+    {
+        public static List<string> GetHashtags(string postString)
+        {
+            var hashtagPattern = new Regex(@"#\w+");
+            var matches = hashtagPattern
+                .Matches(postString).Select(match => match.Value.TrimEnd('.', ',','!','?').ToLower())
+                .Distinct().ToList();
+            return matches;
+        }
+    }
+}
