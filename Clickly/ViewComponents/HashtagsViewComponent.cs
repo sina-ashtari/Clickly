3c535640eca40a81575cc458f06aeda659b0e716
sina-ashtari <sinaa1383@gmail.com>
2025-04-06 18:51:47 +0330

    Feat : Added partial view Hashtags and stories will be part of main page with viewComponents

---
+++ Clickly/ViewComponents/HashtagsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Clickly.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clickly.ViewComponents
+{
+    public class HashtagsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public HashtagsViewComponent(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var oneWeekAgo = DateTime.UtcNow.AddDays(-7);
+            var topThreeHashtags = await _dbContext.Hashtags
+                .Where(h => h.DateCreated >= oneWeekAgo)
+                .OrderByDescending(n => n.Count)
+                .Take(3)
+                .ToListAsync();
+            return View(topThreeHashtags);  
+        }
+    }
+}
---
+++ Clickly/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Clickly.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clickly.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StoriesViewComponent(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _dbContext.Stories.Where(n => n.DateCreated >= DateTime.UtcNow.AddHours(-24)).Include(s => s.User).ToListAsync();
+            return View(allStories);
+        }
+    }
+}
