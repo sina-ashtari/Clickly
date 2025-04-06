b2eb15c851bff8ba2a8dc50695616848a1a3cd07
sina-ashtari <sinaa1383@gmail.com>
2025-04-06 18:41:51 +0330

    Feat : Added Hashtag model class

---
+++ Clickly.Data/Models/Hashtags.cs
@@ -0,0 +1,11 @@
+﻿namespace Clickly.Data.Models
+{
+    public class Hashtags
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int Count { get; set; }
+    }
+}
