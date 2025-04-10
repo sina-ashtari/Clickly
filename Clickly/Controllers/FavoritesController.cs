fc7c1333c80a7957464e3534c738735f3733f5eb
sina-ashtari <sinaa1383@gmail.com>
2025-04-10 12:36:04 +0330

    Feat : Add FavoriteController controller for accessing favorited posts

---
+++ Clickly/Controllers/FavoritesController.cs
@@ -0,0 +1,20 @@
+﻿using Clickly.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly IPostService _postService;
+        public FavoritesController(IPostService postService)
+        {
+            _postService = postService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            int loggedInUserId = 1;
+            var myFavoritePost = await _postService.GetAllFavoritedPostAsync(loggedInUserId);
+            return View(myFavoritePost);
+        }
+    }
+}
