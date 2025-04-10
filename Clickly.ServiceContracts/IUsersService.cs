bafc16692e7640da9c280d695aa29a746a76ea15
sina-ashtari <sinaa1383@gmail.com>
2025-04-10 12:35:05 +0330

    Feat : Add user profile settings

---
+++ Clickly.ServiceContracts/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿
+using Clickly.Data.Models;
+
+namespace Clickly.ServiceContracts
+{
+    public interface IUsersService
+    {
+        Task<User> GetUser(int loggedInUserId);
+        Task UpdateUserProfilePicture(int userId, string profilePictureUrl);
+    }
+}
---
+++ Clickly.Services/UsersService.cs
@@ -0,0 +1,31 @@
+﻿using Clickly.Data;
+using Clickly.Data.Models;
+using Clickly.ServiceContracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clickly.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UsersService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<User> GetUser(int loggedInUserId)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(n => n.Id == loggedInUserId) ?? new User() { FullName =   ""};
+        }
+
+        public async Task UpdateUserProfilePicture(int userId, string profilePictureUrl)
+        {
+            var userDatabase = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            if (userDatabase != null) 
+            {
+                userDatabase.ProfilePictureUrl = profilePictureUrl;
+                _dbContext.Users.Update(userDatabase);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
