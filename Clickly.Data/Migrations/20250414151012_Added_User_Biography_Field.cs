fdfd4686b0485b56c103d318744434ab79fef2ec
sina-ashtari <sinaa1383@gmail.com>
2025-04-14 20:19:51 +0330

    Feat : Add related migration

---
+++ Clickly.Data/Migrations/20250414151012_Added_User_Biography_Field.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clickly.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_User_Biography_Field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "Users");
+        }
+    }
+}
