45aa11e13e4cee92caef2f9613347c85b618b55b
sina-ashtari <sinaa1383@gmail.com>
2025-04-14 20:30:10 +0330

    Refactor : Add base controller. Due to better readability and maintance,

---
+++ Clickly/Controllers/Base/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers.Base
+{
+    public abstract class BaseController : Controller
+    {
+        protected int ? GetUserId()
+        {
+            var loggedInUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(loggedInUser))
+                return null;
+            return int.Parse(loggedInUser);
+        }
+        protected IActionResult RedirectToLogin()
+        {
+            return RedirectToAction("Login", "Authentication");
+        }
+    }
+}
