7a8423d6d63ed49e333257a0a1ae86e546101c6c
sina-ashtari <sinaa1383@gmail.com>
2025-04-14 20:16:23 +0330

    Feat : Add authentication viewmodels

---
+++ Clickly/ViewModels/Authentication/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clickly.ViewModels.Authentication
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid emaill address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Clickly/ViewModels/Authentication/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clickly.ViewModels.Authentication
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage ="First name must contain only letters.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name must contain only letters.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "The confirm password doesn't match password.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
