d0f4b6e346dd2e6188be994782b7bd96fb0670f9
sina-ashtari <sinaa1383@gmail.com>
2025-04-14 20:25:35 +0330

    Feat : Add database identify

--- Clickly.Data/Helper/DbInitializer.cs
+++ Clickly.Data/Helper/DbInitializer.cs
@@ -1,9 +1,57 @@
-﻿using Clickly.Data.Models;
+﻿using Clickly.Data.Helper.Constants;
+using Clickly.Data.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Clickly.Data.Helper
 {
     public static class DbInitializer
     {
+        public static async Task SeedUsersAndRolesAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            // roles
+            if (!roleManager.Roles.Any()) 
+            {
+                foreach (var roleName in AppRoles.All) 
+                {
+                    if (!await roleManager.RoleExistsAsync(roleName)) 
+                    {
+                        await roleManager.CreateAsync(new IdentityRole<int>(roleName));
+                    }
+                }
+            }
+
+            // users with roles
+            if (!userManager.Users.Any(n => !string.IsNullOrEmpty(n.Email))) 
+            {
+                var userPassword = "password";
+                var newUser = new User { 
+                    FullName = "Sina Ashtari",
+                    Email = "email@email.com", 
+                    UserName = "sina",
+                    ProfilePictureUrl = "",
+                    EmailConfirmed = true
+                };
+                var result = await userManager.CreateAsync(newUser, userPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, AppRoles.User);
+                }
+
+                var newAdmin = new User
+                {
+                    FullName = "admin",
+                    Email = "admin@email.com",
+                    ProfilePictureUrl = "",
+                    EmailConfirmed = true
+                };
+                var resultNewAdmin = await userManager.CreateAsync(newAdmin, userPassword);
+                if (resultNewAdmin.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, AppRoles.Admin);
+                }
+            }
+
+        }
         public static async Task SeedAsync(ApplicationDbContext context)
         {
             // in case that we dont have any posts or any users, so we creating a new user with post as initializer.
