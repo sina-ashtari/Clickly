97a4e466156c0ceaf171e21f3e651fda21a05041
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:12:00 +0330

    Feat<user, friends> : Add service injection

--- Clickly/Program.cs
+++ Clickly/Program.cs
@@ -28,6 +28,7 @@ namespace Clickly
             builder.Services.AddScoped<IStoriesService, StoriesService>();
             builder.Services.AddScoped<IFilesService, FilesService>();
             builder.Services.AddScoped<IUsersService, UsersService>();
+            builder.Services.AddScoped<IFriendsService, FriendsService>();
 
             // Identity configuration
             builder.Services.AddIdentity<User, IdentityRole<int>>( options =>
