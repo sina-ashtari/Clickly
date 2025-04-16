b63977419755758ec921c161f8a12db7e20d7be6
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:11:15 +0330

    Feat : Add friendship status

---
+++ Clickly.Data/Helper/Constants/FriendshipStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Clickly.Data.Helper.Constants
+{
+    public static class FriendshipStatus
+    {
+        public const string Pending = "Pending";
+        public const string Accepted = "Accepted";
+        public const string Rejected = "Rejected";
+        public const string Canceled = "Canceled";
+    }
+}
