15fa6def1b5c6d4390c8058432a895d8f5660cd3
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:03:08 +0330

    Feat : Add friendship or connection between users

---
+++ Clickly.Data/Models/Friendship.cs
@@ -0,0 +1,14 @@
+﻿namespace Clickly.Data.Models
+{
+    public class Friendship
+    {
+        public int Id { get; set; }
+        
+        public DateTime DateCreated { get; set; }
+        
+        public int SenderId { get; set; }
+        public virtual User Sender { get; set; }
+        public int ReceiverId { get; set; }
+        public virtual User Receiver { get; set; }
+    }
+}
