65d11de01625f4704ecf1a11cf51e2f5128d3c80
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:10:15 +0330

    Feat : Add user controller for user details

---
+++ Clickly/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Clickly.Data.Models;
+using Clickly.ServiceContracts;
+using Clickly.ViewModels.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly IUsersService _usersService;
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(IUsersService usersService, UserManager<User> userManager)
+        {
+            _usersService = usersService;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int userId) 
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            var userPost = await _usersService.GetUserPosts(userId);
+
+            var userProfileVM = new GetUserProfileVM()
+            {
+                Posts = userPost,
+                User = user
+            };
+            return View(userProfileVM);
+        }
+    }
+}
