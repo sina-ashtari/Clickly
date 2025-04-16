aadf4e208bef7860f0a1a1ae53f054ae1cdcdbdb
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:10:56 +0330

    Feat : Add users with friends DTO

---
+++ Clickly.Data/Dtos/UserWithFriendsDto.cs
@@ -0,0 +1,10 @@
+﻿using Clickly.Data.Models;
+
+namespace Clickly.Data.Dtos
+{
+    public class UserWithFriendsDto
+    {
+        public User User { get; set; }  
+        public int FriendsCount { get; set; }
+    }
+}
