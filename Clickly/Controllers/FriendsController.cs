22399e354c76f7a2cd8e42ee37ff1c490138a1cd
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:09:48 +0330

    Feat : Add friends controller

---
+++ Clickly/Controllers/FriendsController.cs
@@ -0,0 +1,58 @@
+﻿using Clickly.Controllers.Base;
+using Clickly.Data.Helper.Constants;
+using Clickly.ServiceContracts;
+using Clickly.ViewModels.Friends;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers
+{
+
+    public class FriendsController : BaseController
+    {
+        private readonly IFriendsService _friendsService;
+        public FriendsController(IFriendsService friendsService)
+        {
+            _friendsService = friendsService;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var userId = GetUserId();
+            if (!userId.HasValue) RedirectToLogin();
+
+            var friendsData = new FriendshipVM()
+            {
+                FriendRequestsSent = await _friendsService.GetSentFriendRequestAsync(userId.Value),
+                FriendRequestReceived = await _friendsService.GetReceivedFriendRequestAsync(userId.Value),
+                Friends = await _friendsService.GetFriendsAsync(userId.Value),
+            };
+            return View(friendsData);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendFriendRequest(int receiverId) 
+        { 
+            var userId = GetUserId();
+            if (!userId.HasValue) RedirectToLogin();
+
+            await _friendsService.SendRequestAsync(userId.Value, receiverId);
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CancelFriendRequest(int requestId)
+        {
+            await _friendsService.UpdateRequestStatusAsync(requestId, FriendshipStatus.Canceled);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AcceptFriendRequest(int requestId)
+        {
+            await _friendsService.UpdateRequestStatusAsync(requestId, FriendshipStatus.Accepted);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
