03429d6b155c4d9f82c26be5ff5ecf75f8438847
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:01:57 +0330

    Refactor : Change partial view to view component

---
+++ Clickly/ViewComponents/SuggestedFriendsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Clickly.ServiceContracts;
+using Clickly.ViewModels.Friends;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.ViewComponents
+{
+    public class SuggestedFriendsViewComponent : ViewComponent
+    {
+        public readonly IFriendsService _friendsService;
+        public SuggestedFriendsViewComponent(IFriendsService friendsService)
+        {
+            _friendsService = friendsService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var loggedInUserId = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier); 
+            var suggestedFriends = await _friendsService.GetSuggestedFriendsAsync(int.Parse(loggedInUserId));
+            var suggestedFriendsVM = suggestedFriends.Select(n => new UserWithFriendsVM()
+            {
+                UserId = n.User.Id,
+                FullName = n.User.FullName,
+                ProfilePictureUrl = n.User.ProfilePictureUrl,
+                FriendsCount = n.FriendsCount,
+            }).ToList();
+            return View(suggestedFriendsVM);
+        }
+    }
+}
