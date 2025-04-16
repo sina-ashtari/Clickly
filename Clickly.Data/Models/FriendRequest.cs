343dcc7d6e8715f6537a75d5cf8894932b9fc29e
sina-ashtari <sinaa1383@gmail.com>
2025-04-16 19:02:45 +0330

    Feat : Add friend request model class

---
+++ Clickly.Data/Models/FriendRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Clickly.Data.Models
+{
+    public class FriendRequest
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+        public int ReceiverId { get; set; }
+        public User Receiver { get; set; }
+    }
+}
