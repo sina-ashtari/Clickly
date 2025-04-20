33354ff43f400734dd939c9422a221fc3d211afa
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:42:36 +0330

    Feat : Add related database migrations

---
+++ Clickly.Data/Migrations/20250420095248_Added_PostId_To_Notifications_Table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clickly.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_PostId_To_Notifications_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Notifications",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Notifications");
+        }
+    }
+}
