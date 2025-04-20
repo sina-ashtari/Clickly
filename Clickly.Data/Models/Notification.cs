8816bd636c320d3fb066669902aa5f388aa25a20
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:49:47 +0330

    Feat : Add notification model class

---
+++ Clickly.Data/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Clickly.Data.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int? PostId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public string Type { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
