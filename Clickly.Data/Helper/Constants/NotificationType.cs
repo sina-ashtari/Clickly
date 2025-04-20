614e5ba10932da30d317b3d7c08aad152fdb75cf
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:49:26 +0330

    Feat : Add notification types

---
+++ Clickly.Data/Helper/Constants/NotificationType.cs
@@ -0,0 +1,11 @@
+﻿namespace Clickly.Data.Helper.Constants
+{
+    public static class NotificationType
+    {
+        public const string Like = "Like";
+        public const string Comment = "Comment";
+        public const string Favorite = "Favorite";
+        public const string FriendRequest = "FriendRequest";
+        public const string FriendRequestApproved = "FriendRequestApproved";
+    }
+}
