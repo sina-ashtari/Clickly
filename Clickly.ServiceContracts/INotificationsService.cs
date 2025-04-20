9b1bb76895c93ca3a58811b41eec0aed2edbc6da
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:57:07 +0330

    Feat : Add notification service interface

---
+++ Clickly.ServiceContracts/INotificationsService.cs
@@ -0,0 +1,12 @@
+﻿using Clickly.Data.Models;
+
+namespace Clickly.ServiceContracts
+{
+    public interface INotificationsService
+    {
+        Task AddNotificationAsync(int userId, string userFullName, int? postId, string notificationType);
+        Task<int> GetUnreadNotificationsCount(int userId);
+        Task<List<Notification>> GetNotifications(int userId);
+        Task SetNotificationAsReadAsync(int notificationId);
+    }
+}
