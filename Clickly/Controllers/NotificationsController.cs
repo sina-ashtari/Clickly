c78628600a08bb79a98212a67b30bca769f55ef1
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:53:22 +0330

    Feat : Add notifications controller

---
+++ Clickly/Controllers/NotificationsController.cs
@@ -0,0 +1,48 @@
+﻿using Clickly.Controllers.Base;
+using Clickly.Data.Models;
+using Clickly.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers
+{
+    public class NotificationsController : BaseController
+    {
+        private readonly INotificationsService _notificationsService;
+        public NotificationsController(INotificationsService notificationsService) 
+        { 
+            _notificationsService = notificationsService;
+        }
+        
+        public async Task<IActionResult> GetNotifications()
+        {
+            var userId = GetUserId();
+            if (!userId.HasValue) RedirectToLogin();
+            var notifications = await _notificationsService.GetNotifications(userId.Value);
+            return PartialView("Notifications/_Notifications", notifications);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCount()
+        {
+            var userId = GetUserId();
+            if (!userId.HasValue) RedirectToLogin();
+
+            var count = await _notificationsService.GetUnreadNotificationsCount(userId.Value);
+            return Json(count);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SetNotificationAsRead(int notificationId)
+        {
+            var userId = GetUserId();
+            if (!userId.HasValue) RedirectToLogin();
+
+            await _notificationsService.SetNotificationAsReadAsync(notificationId);
+            var notifications = await _notificationsService.GetNotifications(userId.Value);
+            return PartialView("Notifications/_Notifications", notifications);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
