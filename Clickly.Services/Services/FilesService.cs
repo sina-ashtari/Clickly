c4f0195471f792ab3017be280c78564265470257
sina-ashtari <sinaa1383@gmail.com>
2025-04-20 20:44:20 +0330

    Refactor : Change services location to be under "Services" folder.

---
+++ Clickly.Services/Services/FilesService.cs
@@ -0,0 +1,46 @@
+﻿using Clickly.Data.Helper.Enums;
+using Clickly.ServiceContracts;
+using Microsoft.AspNetCore.Http;
+
+namespace Clickly.Services.Services
+{
+    public class FilesService : IFilesService
+    {
+        public async Task<string> UploadImageAsync(IFormFile file, ImageFileType fileType)
+        {
+            string filePathUpload = fileType switch
+            {
+                ImageFileType.PostImage => Path.Combine("images", "posts"),
+                ImageFileType.CoverImage => Path.Combine("images", "covers"),
+                ImageFileType.ProfileImage => Path.Combine("images", "profilePictures"),
+                ImageFileType.StoryImage => Path.Combine("images", "stories"),
+                _ => throw new ArgumentException("Invalid file type")
+
+            };
+
+            if (file != null && file.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                // validating if the provided image type is correct then saving it in wwwroot folder
+                if (file.ContentType.Contains("image"))
+                {
+                    // first we trying to create root folder in that case we dont have it
+                    string rootFolderPathImage = Path.Combine(rootFolderPath, filePathUpload);
+                    Directory.CreateDirectory(rootFolderPathImage);
+
+                    // then creating file name with defined Guid for each of them
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(rootFolderPathImage, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+
+                        return $"/{filePathUpload}/{fileName}";
+                    }
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ Clickly.Services/Services/StoriesService.cs
@@ -0,0 +1,30 @@
+﻿using Clickly.Data;
+using Clickly.Data.Models;
+using Clickly.ServiceContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clickly.Services.Services
+{
+    public class StoriesService : IStoriesService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StoriesService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Story> CreateStoryAsync(Story story)
+        {
+            
+            await _dbContext.Stories.AddAsync(story);
+            await _dbContext.SaveChangesAsync();
+            return story;
+        }
+
+        public async Task<List<Story>> GetAllStoriesAsync()
+        {
+            var allStories = await _dbContext.Stories.Where(n => n.DateCreated >= DateTime.UtcNow.AddHours(-24)).Include(s => s.User).ToListAsync();
+            return allStories;
+        }
+    }
+}
