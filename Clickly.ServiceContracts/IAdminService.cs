bad1cfa51388523a4675b3face1ffe4dd655925c
sina-ashtari <sinaa1383@gmail.com>
2025-04-22 17:19:46 +0330

    Feat : Add admin service interface

---
+++ Clickly.ServiceContracts/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using Clickly.Data.Models;
+
+namespace Clickly.ServiceContracts
+{
+    public interface IAdminService
+    {
+        Task<List<Post>> GetReportedPostAsync();
+        Task ApproveReportAsync(int postId);
+        Task RejectReportAsync(int postId);
+    }
+}
