b1f742929f9829e3ca22ac5c19c421a1049608eb
sina-ashtari <sinaa1383@gmail.com>
2025-04-22 17:15:00 +0330

    Refactor : Change authorize action to only accessible by admins

---
+++ Clickly/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Clickly.Data.Helper.Constants;
+using Clickly.ServiceContracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Controllers
+{
+    [Authorize(Roles = AppRoles.Admin)]
+    public class AdminController : Controller
+    {
+        private readonly IAdminService _adminService;
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var reportedPost = await _adminService.GetReportedPostAsync();
+            return View(reportedPost);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ApproveReport(int postId)
+        {
+            await _adminService.ApproveReportAsync(postId);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> RejectReport(int postId)
+        {
+            await _adminService.RejectReportAsync(postId);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
