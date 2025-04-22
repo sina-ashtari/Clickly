699bc94666a309ccc49ff2a32290e17ea5e8e770
sina-ashtari <sinaa1383@gmail.com>
2025-04-22 17:14:53 +0330

    Refactor : Change authorize action to only accessible by users and admins

--- Clickly/Controllers/AuthenticationController.cs
+++ Clickly/Controllers/AuthenticationController.cs
@@ -49,9 +49,10 @@ namespace Clickly.Controllers
 
             if (result.Succeeded)
             {
-
-
-                return RedirectToAction("Index", "Home");
+                if (User.IsInRole(AppRoles.Admin))
+                    return RedirectToAction("Index", "Admin");
+                else
+                    return RedirectToAction("Index", "Home");
             }
 
             ModelState.AddModelError("", "Invalid login attempt");
