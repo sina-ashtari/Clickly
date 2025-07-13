646d673e4970b6337ff3193eaed78284aece05b4
sina-ashtari <sinaa1383@gmail.com>
2025-07-13 15:49:05 +0330

    Feat : Add User connection partial view

---
+++ Clickly/ViewComponents/FollowersViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Clickly.ServiceContracts;
+using Clickly.ViewModels.Friends;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.ViewComponents
+{
+    public class ConnectionsViewComponent : ViewComponent
+    {
+        private readonly IFriendsService _friendsService;
+        public ConnectionsViewComponent(IFriendsService friendsService)
+        {
+            _friendsService = friendsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int userId)
+        {
+
+            var connections = await _friendsService.GetConnectionsAsync(userId);
+            var result = new List<UserWithFriendsVM>();
+            foreach (var connection in connections) 
+            {
+                result.Add(new UserWithFriendsVM
+                {
+                    UserId = connection.User.Id,
+                    FullName = connection.User.FullName,
+                    ProfilePictureUrl = connection.User.ProfilePictureUrl,
+                    FriendsCount = connection.FriendsCount,
+
+                });
+            }
+            return View(result);
+        }
+    }
+}
