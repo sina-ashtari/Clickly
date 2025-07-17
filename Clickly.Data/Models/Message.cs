73e7ef9d5cf67acd73ded5352df6d307c3b5a85b
sina-ashtari <sinaa1383@gmail.com>
2025-07-17 13:41:58 +0330

    Feat : Add message data class

---
+++ Clickly.Data/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace Clickly.Data.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string? UploadedFileUrl { get; set; } 
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; } = false;
+
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+    }
+}
