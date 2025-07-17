5553f184c281a424c6d7ac2c7ed637485cbfe546
sina-ashtari <sinaa1383@gmail.com>
2025-07-17 13:50:58 +0330

    Feat : Add Chat Service layer. Contains getting all private chat room and messages inside of it

---
+++ Clickly.ServiceContracts/IChatsService.cs
@@ -0,0 +1,10 @@
+﻿using Clickly.Data.Models;
+
+namespace Clickly.ServiceContracts
+{
+    public interface IChatsService
+    {
+        Task<List<Message>> GetMessagesAsync(int currentUserId, int targetUserId);
+        Task<List<Message>> GetPrivateChatsAsync(int userId);
+    }
+}
---
+++ Clickly.Services/Services/ChatsService.cs
@@ -0,0 +1,42 @@
+﻿using Clickly.Data;
+using Clickly.Data.Models;
+using Clickly.ServiceContracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clickly.Services.Services
+{
+    public class ChatsService : IChatsService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ChatsService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Message>> GetMessagesAsync(int currentUserId, int targetUserId)
+        {
+            var messages = await _dbContext.Messages
+                .Where(m => (m.SenderId == currentUserId && m.ReceiverId == targetUserId) || (m.SenderId == targetUserId && m.ReceiverId == currentUserId))
+                .OrderBy(m => m.SentAt)
+                .ToListAsync();
+            return messages;
+        }
+
+        public async Task<List<Message>> GetPrivateChatsAsync(int userId)
+        {
+            var messages = await _dbContext.Messages
+        .Where(m => m.SenderId == userId || m.ReceiverId == userId)
+        .OrderByDescending(m => m.SentAt)
+        .Include(m => m.Sender)
+        .Include(m => m.Receiver)
+        .ToListAsync();
+
+            var latestMessages = messages
+                .GroupBy(m => m.SenderId == userId ? m.ReceiverId : m.SenderId)
+                .Select(g => g.First())
+                .ToList();
+
+            return latestMessages;
+        }
+    }
+}
