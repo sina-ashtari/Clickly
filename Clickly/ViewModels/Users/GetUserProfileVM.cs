822b57babd8a1b2644682bb0dd4a1083f5ba73fc
sina-ashtari <sinaa1383@gmail.com>
2025-07-17 13:48:45 +0330

    Feat : Add current user id section inside the get user profile view model

--- Clickly/ViewModels/Users/GetUserProfileVM.cs
+++ Clickly/ViewModels/Users/GetUserProfileVM.cs
@@ -4,6 +4,7 @@ namespace Clickly.ViewModels.Users
 {
     public class GetUserProfileVM
     {
+        public int CurrentUserId { get; set; }
         public List<Post> Posts { get; set; }
         public User User { get; set; }
     }
