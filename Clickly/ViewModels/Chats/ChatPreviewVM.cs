4d7d99eb534d01cb3d8597fb3302c0ee72f30e27
sina-ashtari <sinaa1383@gmail.com>
2025-07-17 13:45:32 +0330

    Feat : Add chat preview viewmodel, related information inside the all private chat screen

---
+++ Clickly/ViewModels/Chats/ChatPreviewVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Clickly.ViewModels.Chats
+{
+    public class ChatPreviewVM
+    {
+        public int Id { get; set; }
+        public int PartnerId { get; set; }
+        public string PartnerName { get; set; }
+        public string PartnerProfilePicture { get; set; }
+        public string LastMessage { get; set; }
+        public DateTime SentAt { get; set; }
+    }
+}
