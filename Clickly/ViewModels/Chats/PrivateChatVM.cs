2f65f6dffff88356b8ab21256dc195f07433bcc8
sina-ashtari <sinaa1383@gmail.com>
2025-07-17 13:44:56 +0330

    Feat : Add private chat view model, related informations inside chat view page

---
+++ Clickly/ViewModels/Chats/PrivateChatVM.cs
@@ -0,0 +1,15 @@
+﻿using Clickly.Data.Models;
+
+namespace Clickly.ViewModels.Chats
+{
+    public class PrivateChatVM
+    {
+        public int CurrentUserId { get; set; }
+        public int TargetUserId { get; set; }
+        public string? TargetFullName { get; set; }
+        public string? CurrentFullName { get; set; }
+        public string? TargetProfilePicture { get; set; }
+        public string? CurrentProfilePicture { get; set; }
+        public List<Message>? Messages { get; set; }
+    }
+}
